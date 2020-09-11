9b7945ac0b6a215a93f059b3d4d3f7bcd1239c51
7J647 <007J647@gmail.com>
2020-09-10 23:17:04 -0400

    made a class of 'rows' in the main container and used a forLoop to create var 'hourRow', populated each row with the corresponding hour of the scheduler

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
 
     var container = $(".container");
+    var rows = $(".rows");
 
-    var businessHour = ["9AM", "10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM","5PM"]
+    var businessHours = ["9AM", "10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM", "5PM"]
 
-        for (var i=0; i < businessHour.length; i++) {
+        for (var i=0; i < businessHours.length; i++) {
             // tested the forLoop
-            // console.log(businessHour[i]);
-            // var hourRow = document.createElement ("div");
+            // console.log(businessHours[i]);
+            var hourRow = $("<div>");
+            hourRow.text(businessHours[i]);
+            rows.append(hourRow);
+            console.log(hourRow);
 
 
 
