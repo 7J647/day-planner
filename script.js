fa32482943dcce719d5b95793d5df39f7447c0aa
7J647 <007J647@gmail.com>
2020-09-10 23:01:14 -0400

    set up an array called businessHour and tested them with a for Loop

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
 
+    var container = $(".container");
+
+    var businessHour = ["9AM", "10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM","5PM"]
+
+        for (var i=0; i < businessHour.length; i++) {
+            // tested the forLoop
+            // console.log(businessHour[i]);
+            // var hourRow = document.createElement ("div");
+
+
+
+        }
 
 
 
 
 
 
-
-
-    
 })
