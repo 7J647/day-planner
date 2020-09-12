ca4613934838ac268da88bc4d31debe13c557fc0
7J647 <007J647@gmail.com>
2020-09-11 22:40:03 -0400

    tidied up pseudocode with explanations of efforts made to retrieve local storage so that the tasks would persist on the page when refreshed

--- script.js
+++ script.js
@@ -9,9 +9,8 @@ $(document).ready(function(){
        
     var container = $(".container");
 
+    //build array for a for Loop to iterate over and create rows
     var businessHours = [{businessTime: "9AM", militaryTime:9}, {businessTime: "10AM", militaryTime:10}, {businessTime: "11AM", militaryTime:11}, {businessTime: "12PM", militaryTime:12}, {businessTime: "1PM", militaryTime: 13}, {businessTime: "2PM", militaryTime:14}, {businessTime: "3PM", militaryTime:15}, {businessTime: "4PM", militaryTime:16}, {businessTime: "5PM", militaryTime:17} ,{businessTime: "6PM", militaryTime:18} ,{businessTime: "7PM", militaryTime:19},{businessTime: "8PM", militaryTime:20},{businessTime: "9PM", militaryTime:21},{businessTime: "10PM", militaryTime:22},{businessTime: "11PM", militaryTime:23}]
-
-    
 
         for (var i=0; i < businessHours.length; i++) {
             
@@ -25,16 +24,11 @@ $(document).ready(function(){
             hourNumber.attr("class", "col-sm-1");
             hourRow.append(hourNumber);
 
-            // parseInt(hour.text) or
-            
-            // var hourNumber = parseInt(businessHours[i])
             var hourNumber = businessHours[i].militaryTime
             var hourNumber = parseInt(hourNumber);
 
-            console.log (actualHour, hourNumber);
+            // console.log (actualHour, hourNumber);
             
-
-             
 
             //if hour is < (before) the current hour, color is xxxx
             //else if hour is the current hour, color is yyyy
@@ -43,7 +37,7 @@ $(document).ready(function(){
             var schedule = $("<input>");
 
 
-            schedule.attr("style", "float:left; width:100%; height:80px;");
+            schedule.attr("style", "float:left; width:100%; height:80px; border-radius: 15px;");
             schedule.attr("class", "col-sm-10");
             hourRow.append(schedule);
 
@@ -56,24 +50,47 @@ $(document).ready(function(){
             else if (hourNumber === actualHour) {
                 schedule.attr("style", "float:left; width:100%; height:80px; background-color:#ff6961; color:white");
             // color of the main schedule input field equals the current hour color
-            // schedule.attr("background-color:#77dd77; color:white");
-            
+           
             }
             
             else  {
                 // color of the main schedule input field is greater than the after hour color
-                // schedule.attr("background-color:#77dd77; color:white");
                 schedule.attr("style", "float:left; width:100%; height:80px; background-color:#77dd77; color:white");
 
             }
             
-            //populating the input from local storage if anything is in there
+            // FROM HERE I TRIED TO SET UP LOCAL STORAGE.  I WAS ABLE TO GET IT IN TO LOCAL STORAGE BUT NOT OUT.
+            
+            //EXPERIMENTED WITH SETTING NEW VARIABLES
             // var existingTasks = localStorage.getItem(hourNumber);
             // schedule.val(existingTasks);
 
             // var schedule = localStorage.getItem(hourNumber);
             // schedule.val(existingTasks);
             // schedule = storage.getItem(hourNumber);
+
+            //EXPERIMENTED WITH FUNCTIONS AND JSON
+                // function getSavedTasks () {
+                //     var schedule = JSON.parse(localStorage.getItem("input"));
+
+
+                // }
+
+                // function saveTasks () {
+                //     localStorage.setItem("hourNumber"),JSON.stringify({
+                //      schedule: $(this).siblings("input");,
+                //      hourNumber: $(this).siblings("div").text();   
+                //     })
+
+
+                // }
+
+            //// EXPERIMENTED WITH ADDING ANOTHER INPUT ONTO THE INPUT VARIABLE
+                // var existingTasks = $("<input>");
+                // var existingTasks = localStorage.getItem(hourNumber, schedule);
+                // schedule.append(existingTasks);
+                // console.log(existingTasks);
+    
            
             var saveButton = $("<button>");
 
@@ -92,7 +109,6 @@ $(document).ready(function(){
 
 
 //input box text "to do" goes into local storage and then it persists.
-//add another to do as necessary in the same hours and that persists as well.
 
     saveButton.on("click", function(){ 
     // console.log("click happened");
@@ -101,47 +117,14 @@ $(document).ready(function(){
     var schedule =$(this).siblings("input");
     var hourNumber =$(this).siblings("div").text();
     
+
     localStorage.setItem(hourNumber, schedule.val());
-   
 
-
-// var HighScore = localStorage.getItem('highScore');
-// if (HighScore == null || HighScore == "null") {
-//   HighScore = 0;
-// }
-
-// if (user.points > HighScore) {
-//   highScore = parseInt(HighScore);
-// }
-// return highScore 
-
-    // localStorage.setItem($(this).parent())
-    
-    // var schedule = JSON.parse(localStorage.getItem(hourNumber);
-   
-
-    // localStorage.getItem(hourNumber, schedule.val(existingTasks));
-
-    //so that the save button knows to save the input right next to it
-    // var schedule =$(this).siblings("input");
-    
-    // // so that the save button knows to pair the input with the hour
-    // var hourNumber =$(this).siblings("button").text(); 
-
-    // //setting the input into local storage, will pull out by key existingTasks
-    localStorage.getItem(hourNumber, schedule.val());
-
-    
+    //LAST DITCH EFFORT TO TRY SOMETHING SIMPLE AND SEE IT WORKED
+    // schedule.getItem(hourNumber);
 
 })
        
-
-
-        }
-
-
-
-
-
+}
 
 })
